a8b5169c6febd5d68cbca5da5fab65c81e9f9a0a
JKillian <jasonkillian35@gmail.com>
2015-02-03 19:38:44 -0500

    Update to reflect new structure of extraction lib

--- extractor.py
+++ extractor.py
@@ -1,5 +1,7 @@
 from extraction.core import ExtractionRunner
-from extraction.runnables import Extractor, RunnableError, Filter
+from extraction.runnables import Extractor, RunnableError, Filter, ExtractorResult
+import defusedxml.ElementTree as safeET
+import xml.etree.ElementTree as ET
 import extraction.utils as utils
 import subprocess32 as subprocess
 import os
@@ -19,9 +21,8 @@ class GrobidExtractor(Extractor):
 
       if resp.status_code != 200:
          raise RunnableError('Grobid returned status {0}'.format(resp.status_code))
-      results = resp.text
-      result_str = '\n'.join(results.split('\n')[1:])
-      return result_str
+      xml_snippet = safeET.fromstring(resp.text.encode('utf-8'))
+      return ExtractorResult(xml_snippet)
 
 class PlainTextExtractor(Extractor):
    @staticmethod
@@ -29,10 +30,15 @@ class PlainTextExtractor(Extractor):
       return [GrobidExtractor]
 
    def extract(self, data, dep_results):
-      xml_text = dep_results[GrobidExtractor]
+      xml_doc = dep_results[GrobidExtractor].xml_result
+      xml_text = ET.tostring(xml_doc)
       remove_tags = re.compile(r'\s*<.*?>', re.DOTALL | re.UNICODE)
       plain_text = remove_tags.sub('\n', xml_text)
-      return plain_text
+
+      root=ET.Element('file')
+      root.text = 'plain_text.txt'
+
+      return ExtractorResult(xml_result = root, files = {'plain_text.txt': plain_text})
 
 
 class AcademicPaperFilter(Filter):
@@ -41,7 +47,7 @@ class AcademicPaperFilter(Filter):
       return [PlainTextExtractor]
 
    def filter(self, data, dep_results):
-      plain_text = dep_results[PlainTextExtractor]
+      plain_text = dep_results[PlainTextExtractor].files['plain_text.txt']
       return  ('REFERENCES' in plain_text or
                'References' in plain_text or
                'Bibliography' in plain_text or
@@ -59,13 +65,13 @@ class TableExtractor(Extractor):
 
 if __name__ == '__main__':
    runner = ExtractionRunner()
-   runner.add_runnable(GrobidExtractor, include_in_output=False)
+   runner.add_runnable(GrobidExtractor )
    runner.add_runnable(PlainTextExtractor)
    runner.add_runnable(AcademicPaperFilter)
    runner.add_runnable(TableExtractor)
 
    file_path = os.path.expanduser('~/testpdfs/012.251.000.pdf')
-   print runner.run_from_file(file_path, pretty=True)
+   runner.run_from_file(file_path)
 
 
 
