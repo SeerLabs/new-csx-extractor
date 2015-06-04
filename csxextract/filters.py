e5b363a63cd8bc609df86e3b122d4ea7a38767db
JKillian <jasonkillian35@gmail.com>
2015-06-04 18:05:40 -0400

    fix import bug

--- csxextract/filters.py
+++ csxextract/filters.py
@@ -1,7 +1,8 @@
 from extraction.runnables import Filter, RunnableError
 import csxextract.config as config
 import extraction.utils
-import interfaces
+import csxextract.interfaces as interfaces
+import subprocess32 as subprocess
 import tempfile
 import shutil
 
