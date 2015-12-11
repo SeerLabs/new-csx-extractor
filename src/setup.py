a25cbea17fba3947e25a8aacdbd66358a7151373
huaiyudavid <davidyangrocs@gmail.com>
2015-12-10 23:33:31 -0600

    setup change

--- src/setup.py
+++ src/setup.py
@@ -4,6 +4,6 @@ setup(name="Extractor Modules",
     version="0.1",
     description="Extractor modules to run with extraction framework",
     author="Huaiyu Yang",
-    packages=['extractor'],
+    packages=['extractor', 'extractor.csxextract', 'extractor.csxextract.extractors', 'extractor.python_wrapper'],
     requires=[]
     )
